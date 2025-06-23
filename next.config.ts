9c726268617363df2547f03c41cfcac2320c56e9
hetu-music <216563469+hetu-music@users.noreply.github.com>
2025-06-23 21:33:50 +0800

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cover.hetu-music.com',
+        hostname: '*.hetu-music.com',
       },
       {
         protocol: 'https',
