34e3b1be267fe4dc5ce4dab91b4e7c587f91793a
hetu-music <216563469+hetu-music@users.noreply.github.com>
2025-12-21 05:15:10 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cover.hetu-music.com",
       },
-      {
-        protocol: "https",
-        hostname: "www.bing.com",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
     ],
     minimumCacheTTL: 3600,
     // 优化图片处理以减少内存占用
