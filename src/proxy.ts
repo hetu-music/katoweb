8b9502b3e4dc1696000fcc9bbd9c5d47caf8a4d8
hetu-music <216563469+hetu-music@users.noreply.github.com>
2025-12-22 09:57:20 +0800

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -29,7 +29,7 @@ export async function proxy(request: NextRequest) {
     cspHeader = `
       default-src 'self';
       script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'sha256-n46vPwSWuMC0W703pBofImv82Z26xo4LXymv0E9caPk=' https://challenges.cloudflare.com https://static.cloudflareinsights.com;
-      style-src 'self' 'unsafe-inline';
+      style-src 'self';
       img-src 'self' blob: data: https://cover.hetu-music.com;
       font-src 'self';
       object-src 'none';
@@ -49,7 +49,7 @@ export async function proxy(request: NextRequest) {
     cspHeader = `
       default-src 'self';
       script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com https://static.cloudflareinsights.com;
-      style-src 'self' 'unsafe-inline';
+      style-src 'self';
       img-src 'self' blob: data: https://cover.hetu-music.com;
       font-src 'self';
       object-src 'none';
